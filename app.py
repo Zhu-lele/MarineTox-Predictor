ee2ebbcd18e4486d253a84bf75f30803967a7795
Zhulab <Zhu_lll@163.com>
2025-07-08 16:09:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="MarineTox Predictor", layout="wide")
 # 加载数据
 @st.cache_data
 def load_data():
-    file_path = os.path.join(os.path.dirname(__file__), "chemicalhazarddataset-20241231.xlsx")
+    file_path = os.path.join(os.path.dirname(__file__), "chemicalhazarddataset-20250708.xlsx")
     if os.path.exists(file_path):
         try:
             return pd.read_excel(file_path, engine="openpyxl")
