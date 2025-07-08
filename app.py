07aed2df6021834bcc24cf7dee48e348c8b6d3a7
Zhulab <Zhu_lll@163.com>
2025-07-08 16:14:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ if selected_value:
             # ✅ NOEC Values 表格，标题不显示
            # 正确地提取 NOEC 列并去除列名中的 .1 后缀
            noec_cols = df.columns[23:27].tolist()
-           clean_noec_species = [col.split('.')[0].strip() for col in noec_cols]
+           #clean_noec_species = [col.split('.')[0].strip() for col in noec_cols]
 
            noec_df = pd.DataFrame({
            "Species": clean_noec_species,
